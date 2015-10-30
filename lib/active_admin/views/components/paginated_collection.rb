3688227408ad97467bc6104c1050ec57be98ea73
Anton Shishkin <shishkin@tprs.ru>
2015-10-30 18:38:04 +0300

    pagination fix

--- lib/active_admin/views/components/paginated_collection.rb
+++ lib/active_admin/views/components/paginated_collection.rb
@@ -76,10 +76,22 @@ module ActiveAdmin
       end
 
       def build_pagination
-        options =  request.query_parameters.except(:commit, :format)
+        options = {}
+        options[:params]     = @params     if @params
         options[:param_name] = @param_name if @param_name
 
-        text_node paginate(collection, options.symbolize_keys)
+        if !@display_total && collection.respond_to?(:offset)
+          # The #paginate method in kaminari will query the resource with a
+          # count(*) to determine how many pages there should be unless
+          # you pass in the :total_pages option. We issue a query to determine
+          # if there is another page or not, but the limit/offset make this
+          # query fast.
+          offset = collection.offset(collection.current_page * @per_page.to_i).limit(1).count
+          options[:total_pages] = collection.current_page + offset
+          options[:right] = 0
+        end
+
+        text_node paginate collection, options
       end
 
       include ::ActiveAdmin::Helpers::Collection
@@ -91,30 +103,38 @@ module ActiveAdmin
           entry_name   = options[:entry_name]
           entries_name = options[:entries_name] || entry_name.pluralize
         elsif collection_is_empty?
-          entry_name   = I18n.t "active_admin.pagination.entry", :count => 1, :default => 'entry'
-          entries_name = I18n.t "active_admin.pagination.entry", :count => 2, :default => 'entries'
+          entry_name   = I18n.t "active_admin.pagination.entry", count: 1, default: 'entry'
+          entries_name = I18n.t "active_admin.pagination.entry", count: 2, default: 'entries'
         else
           key = "activerecord.models." + collection.first.class.model_name.i18n_key.to_s
-          entry_name   = I18n.translate key, :count => 1,               :default => collection.first.class.name.underscore.sub('_', ' ')
-          entries_name = I18n.translate key, :count => collection.size, :default => entry_name.pluralize
+          entry_name   = I18n.translate key, count: 1,               default: collection.first.class.name.underscore.sub('_', ' ')
+          entries_name = I18n.translate key, count: collection.size, default: entry_name.pluralize
         end
 
-        if collection.num_pages < 2
-          case collection_size
-          when 0; I18n.t('active_admin.pagination.empty',    :model => entries_name)
-          when 1; I18n.t('active_admin.pagination.one',      :model => entry_name)
-          else;   I18n.t('active_admin.pagination.one_page', :model => entries_name, :n => collection.total_count)
+        if @display_total
+          if collection.num_pages < 2
+            case collection_size
+            when 0; I18n.t("active_admin.pagination.empty",    model: entries_name)
+            when 1; I18n.t("active_admin.pagination.one",      model: entry_name)
+            else;   I18n.t("active_admin.pagination.one_page", model: entries_name, n: collection.total_count)
+            end
+          else
+            offset = (collection.current_page - 1) * collection.limit_value
+            total  = collection.total_count
+            I18n.t "active_admin.pagination.multiple",
+                   model: entries_name,
+                   total: total,
+                   from: offset + 1,
+                   to: offset + collection_size
           end
         else
+          # Do not display total count, in order to prevent a `SELECT count(*)`.
+          # To do so we must not call `collection.num_pages`
           offset = (collection.current_page - 1) * collection.limit_value
-          if @display_total
-            total  = collection.total_count
-            I18n.t 'active_admin.pagination.multiple', :model => entries_name, :total => total,
-              :from => offset + 1, :to => offset + collection_size
-          else
-            I18n.t 'active_admin.pagination.multiple_without_total', :model => entries_name,
-              :from => offset + 1, :to => offset + collection_size
-          end
+          I18n.t "active_admin.pagination.multiple_without_total",
+                 model: entries_name,
+                 from: offset + 1,
+                 to: offset + collection_size
         end
       end
 
